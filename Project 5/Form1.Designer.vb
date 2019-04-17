b25ea90fa64c1cdb746ac255960f6311bea3e036
Enosh Gomez <datechgeek777@gmail.com>
2019-04-17 19:35:52 -0400

    Project 5 Files

---
+++ Project 5/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMain = New System.Windows.Forms.Button()
+        Me.lblMain = New System.Windows.Forms.Label()
+        Me.txtMain = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnMain
+        '
+        Me.btnMain.BackColor = System.Drawing.Color.Blue
+        Me.btnMain.Font = New System.Drawing.Font("Impact", 33.0!)
+        Me.btnMain.ForeColor = System.Drawing.Color.White
+        Me.btnMain.Location = New System.Drawing.Point(12, 189)
+        Me.btnMain.Name = "btnMain"
+        Me.btnMain.Size = New System.Drawing.Size(632, 127)
+        Me.btnMain.TabIndex = 0
+        Me.btnMain.Text = "Check"
+        Me.btnMain.UseVisualStyleBackColor = False
+        '
+        'lblMain
+        '
+        Me.lblMain.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.lblMain.AutoSize = True
+        Me.lblMain.BackColor = System.Drawing.Color.Red
+        Me.lblMain.Font = New System.Drawing.Font("Times New Roman", 19.0!)
+        Me.lblMain.ForeColor = System.Drawing.Color.White
+        Me.lblMain.Location = New System.Drawing.Point(272, 94)
+        Me.lblMain.Name = "lblMain"
+        Me.lblMain.Size = New System.Drawing.Size(127, 29)
+        Me.lblMain.TabIndex = 1
+        Me.lblMain.Text = "Last Name"
+        Me.lblMain.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'txtMain
+        '
+        Me.txtMain.Font = New System.Drawing.Font("Garamond", 24.0!)
+        Me.txtMain.Location = New System.Drawing.Point(168, 126)
+        Me.txtMain.Name = "txtMain"
+        Me.txtMain.Size = New System.Drawing.Size(336, 43)
+        Me.txtMain.TabIndex = 2
+        '
+        'frm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.ClientSize = New System.Drawing.Size(656, 339)
+        Me.Controls.Add(Me.txtMain)
+        Me.Controls.Add(Me.lblMain)
+        Me.Controls.Add(Me.btnMain)
+        Me.Name = "frm"
+        Me.Text = "The Fifth!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMain As Button
+    Friend WithEvents lblMain As Label
+    Friend WithEvents txtMain As TextBox
+End Class
---
+++ Project 5/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class frm
+    Private Sub btnMain_Click(sender As Object, e As EventArgs) Handles btnMain.Click
+        If btnMain.Text = "Check" Or btnMain.Text = "Incorrect, reenter." Then
+            checkNames()
+        ElseIf btnMain.Text = "Enter" Or btnMain.Text = "Incorrect, please try again." Then
+            checkNumber()
+        ElseIf btnMain.Text = "Correct!" Then
+            emptyText()
+        End If
+
+    End Sub
+
+    'Write a visual basic program that has one label, and one textbox and a button.
+    'The label starts With “Last name".
+    'The textbox start out blank. 
+    'The button should be labeled "check".
+    'If the user enters the programmer's (that’s you) last name in the textbox and clicks the check button then 
+    'the label changes To "N-number" And the button label becomes "enter",  
+    'If the user clicks the check button And the last name Is wrong Then 
+    'the button 's label changes to "incorrect reenter".  
+    'Once the correct last name Is entered Then
+    'If the user enters the programmer's (that's you) N-number in the textbox and clicks the button
+    'the textbox becomes read only And the button's label becomes "correct", 
+    'If the N-number In the textbox Is wrong 
+    'the button's label becomes "incorrect try again" and everything else remains the same.
+
+    Function checkNames()
+        If String.Equals(txtMain.Text, "Gomez") Then
+            btnMain.Text = "Enter"
+            lblMain.Text = "N-Number"
+        Else
+            btnMain.Text = "Incorrect, reenter."
+        End If
+
+    End Function
+
+    Function checkNumber()
+        If String.Equals(txtMain.Text, "Gomez N00875564") Then
+            btnMain.Text = "Correct!"
+        Else
+            btnMain.Text = "Incorrect, please try again."
+        End If
+
+        If String.Equals(txtMain.Text, "") Then
+            Reset()
+        End If
+
+    End Function
+
+    Function emptyText()
+        txtMain.Text = ""
+        btnMain.Text = "Check"
+        lblMain.Text = "Last Name"
+    End Function
+
+End Class
